68f96f3354a7f3c3665a9399493b34364e3a3a8c
vagrant <vagrant@localhost.localdomain>
2020-01-13 09:05:06 +0000

    initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,18 @@
+package 'vim-enhanced'
+package 'nano'
+package 'ntp'
+
+
+package 'tree' do
+  action :install
+end
+
+package 'git' do
+  action :install
+end
+
+file '/etc/motd' do
+  content 'Property of DJ'
+  owner 'root'
+  group 'root'
+end
